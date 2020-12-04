0c47bea32daf23cc4eba7c415281c3390a1a843d
Kingsssman <evgeniy.davydovsky@mail.ru>
2020-12-04 15:31:40 +0300

    feat: add footer

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
-    return <div></div>;
+    return (
+        <div className="main-footer">
+            <nav>
+                <ul className="main-footer__links">
+                    <li className="main-footer__link">
+                        <a href="">TRAVELAND</a>
+                    </li>
+                    <li className="main-footer__link">
+                        <a href="">Our Tours</a>
+                    </li>
+                    <li className="main-footer__link">
+                        <a href="">Sign In</a>
+                    </li>
+                    <li className="main-footer__link">
+                        <a href="">Sign Out</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Footer;
